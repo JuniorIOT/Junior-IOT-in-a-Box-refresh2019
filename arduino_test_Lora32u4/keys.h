d8e016c9639525c56a83405a2772705e80d5a076
tvdtogt <ted@vandertogt.nl>
2019-05-19 20:35:02 +0200

    Delete keys.h

--- arduino_test_Lora32u4/keys.h
+++
@@ -1,4 +0,0 @@
-// put your own keys here , paste from TTN console
-const char *devAddr = "00001111";
-const char *nwkSKey = "AAAABBBBCCCCDDDDAAAABBBBCCCCDDDD";
-const char *appSKey = "AAAABBBBCCCCDDDDAAAABBBBCCCCDDDD";
