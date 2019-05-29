800373bd9397922219e3d23b670e94ac66a9754e
tvdtogt <ted@vandertogt.nl>
2019-05-29 22:15:07 +0200

    added dummy keys

---
+++ arduino_juniorIOT_Lora32u4_plus/keys.h
@@ -0,0 +1,4 @@
+// put your own keys here , paste from TTN console
+const char *devAddr = "00001111";
+const char *nwkSKey = "AAAABBBBCCCCDDDDAAAABBBBCCCCDDDD";
+const char *appSKey = "AAAABBBBCCCCDDDDAAAABBBBCCCCDDDD";
