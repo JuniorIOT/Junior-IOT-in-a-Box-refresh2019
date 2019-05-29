c46b3dc219bb12b53bd0fd02e1464c8ce0056dd6
tvdtogt <ted@vandertogt.nl>
2019-05-29 22:10:05 +0200

    Delete keys.h

--- arduino_juniorIOT_Lora32u4_basic/keys.h
+++
@@ -1,4 +0,0 @@
-// put your own keys here , paste from TTN console
-const char *devAddr = "260117AC";
-const char *nwkSKey = "FEDEE264DC6B976DA694AA91360F08CF";
-const char *appSKey = "C4AA1086E6BB0DAD70CE222FF6BCFC43";
